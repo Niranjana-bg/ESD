9dc210b25d46f343a7e4c658d9b9d5374844e849
Niranjana-bg <72258947+Niranjana-bg@users.noreply.github.com>
2021-03-30 16:05:39 +0530

    Program1
    1.JAVA program to check whether a given number is ARMSTRONG or not. (3 digit)

--- Armstrong.java
+++ Armstrong.java
@@ -1,29 +1,26 @@
 import java.util.Scanner;
+public class Armstrong {
 
-public class Armstrong
-{
-	public static void main (String  args[])
-	{
-		Scanner sc = new Scanner(System.in);
-		System.out.println("enter the number\n");
-		int data = sc.nextInt();
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number to check");
+        
+        int number=sc.nextInt();
 
-		int remainder, result =0;
-		int number = data;
-		while(data !=0)
-		{
-			remainder = data % 10;
+        int originalNumber, remainder, result = 0;
+
+        originalNumber = number;
+
+        while (originalNumber != 0)
+        {
+            remainder = originalNumber % 10;
             result += Math.pow(remainder, 3);
-            data /= 10;
-		}
+            originalNumber /= 10;
+        }
 
-		if(result == number)
-		{
-			System.out.println(number + " is an armstrong number");
-		}
-		else
-		{
-			System.out.println(number +" is not an armstrong number");
-		}
-	}
+        if(result == number)
+            System.out.println(number + " is an Armstrong number.");
+        else
+            System.out.println(number + " is not an Armstrong number.");
+    }
 }
